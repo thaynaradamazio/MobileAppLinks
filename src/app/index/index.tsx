d28ba4acea69c7c015e8dd859a16d1c39bc51b1f
thaynara <thaydamazio2015@gmail.com>
2025-03-19 23:24:39 -0300

    feat: add app principal content

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,14 +1,74 @@
-import { Text, View } from "react-native";
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+
+import { Link } from "@/components/link";
+import { Option } from "@/components/option";
+import { Categories } from "@/components/categories";
 
 export default function Index() 
 {
     return(
         <View style = {styles.container}>
-        <Text style = {styles.title}>Hello</Text>
-        <Text style = {styles.title}>World</Text>
-        <Text>My app</Text>
+
+            <View style={styles.header}>
+            <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+            <TouchableOpacity activeOpacity={0.3}>
+                <MaterialIcons name="add" size={32} color={colors.green[300]} />
+            </TouchableOpacity>
+            </View>
+
+            <Categories />
+
+            <FlatList
+            data={["1", "2", "3"]}
+            keyExtractor={(item) => item}
+            renderItem={() => (
+                <Link 
+                    name="Google"
+                    url="https://google.com" 
+                    onDetails={() => console.log("Clicou!")} 
+                />
+            )}
+
+            style={styles.links}
+            contentContainerStyle={styles.linksContent}
+            showsVerticalScrollIndicator={false}
+            />
+
+            <Modal transparent visible={true} >
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+                            <TouchableOpacity>
+                            <MaterialIcons 
+                            name="close" 
+                            size={20} 
+                            color={colors.gray[400]} 
+                            />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.modalLinkName}>
+                            Google
+                        </Text>
+
+                        <Text style={styles.modalURL}>
+                        https://google.com
+                        </Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Abrir" icon="language" />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+            
         </View>
     )
 }
--- src/app/index/styles.tsx
+++ src/app/index/styles.tsx
@@ -1,15 +1,76 @@
 import { StyleSheet } from "react-native";
 
-import { colors } from "../../styles/colors";
+import { colors } from "@/styles/colors";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingTop: 62,
     },
     title: {
         color: colors.green[900],
         fontSize: 22,
     },
+    header: {
+        paddingHorizontal: 24,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 32,
+    },
+    logo: {
+        height: 32,
+        width: 38,
+    },
+    links:{
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[600],
+    },
+    linksContent:{
+        gap: 20,
+        padding: 24,
+        paddingBottom: 100,
+    },
+    modal:{
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+    modalContent:{
+        backgroundColor: colors.gray[900],
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[800],
+        paddingBottom: 42,
+        padding: 24,
+    },
+    modalHeader:{
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 32,
+    },
+    modalCategory:{
+        flex: 1,
+        fontSize: 16,
+        fontWeight: "500",
+        color: colors.gray[400],
+    },
+    modalLinkName:{
+        color: colors.gray[200],
+        fontSize: 18,
+        fontWeight: "600",
+    },
+    modalURL:{
+        color: colors.gray[400],
+        fontSize: 14,
+    },
+    modalFooter:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 32,
+        width: "100%",
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[600],
+        paddingVertical: 14
+    }
 })
